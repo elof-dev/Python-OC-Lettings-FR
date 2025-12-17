650a233a1f974fd58f466730339f9366db61c4a7
elof-dev <elodie.fourcade65@gmail.com>
2025-12-17 19:44:16 +0700

    CI: update staticfile

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -130,4 +130,4 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
